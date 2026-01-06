0fdde6dbf5281215c34fcd12e3c4c9606b427518
AmjedBS <amjedbenslimene@gmail.com>
2026-01-06 16:07:58 +0100

    uloading data into project
    add json file and add images folder with content

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Menu from './Views/Menu/Menu.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>hello world!</h1>
+    <Menu />
   </StrictMode>,
 )
